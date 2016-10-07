74c200b8b29302f66c6ad0e0becdd6b15ca2b256
fqian <fqian91@gmail.com>
2016-10-07 14:39:41 -0400

    finished hw5

---
+++ lc.py
@@ -0,0 +1,91 @@
+'''
+    LightCurve module
+'''
+
+import itertools
+import collections
+import os
+from collections import defaultdict
+import reprlib
+
+def lc_reader(filename):
+    lclist=[]
+    with open(filename) as fp:
+        for i, line in enumerate(fp):
+            if i==0:
+                keys = line.split()
+            if i==1:
+                values = line.split()
+            if line.find('#')!=0:
+                lclist.append([float(f) for f in line.strip().split()])
+        dict_of_facets=dict(zip(keys, values))
+    return lclist, dict_of_facets
+
+class LightCurve(collections.abc.Sequence):
+    def __init__(self, data, metadict):
+        self._time = [x[0] for x in data]
+        self._amplitude = [x[1] for x in data]
+        self._error = [x[2] for x in data]
+        self.metadata = metadict
+        self.filename = None
+        self._timeseries = list(zip(self._time, self._amplitude))
+    
+    @classmethod
+    def from_file(cls, filename):
+        lclist, metadict = lc_reader(filename)
+        instance = cls(lclist, metadict)
+        instance.filename = filename
+        return instance
+    
+    def __repr__(self):
+        class_name = type(self).__name__
+        components = reprlib.repr(list(itertools.islice(self.timeseries,0,10)))
+        components = components[components.find('['):]
+        return '{}({})'.format(class_name, components)    
+    
+    def __getitem__(self, pos):
+        return self._timeseries[pos]
+    
+    def __len__(self):
+        return len(self._timeseries)
+        
+    #your code here
+    @property
+    def time(self):
+        return self._time
+    
+    @property
+    def amplitude(self):
+        return self._amplitude
+    
+    @property
+    def timeseries(self):
+        return self._timeseries
+
+class LightCurveDB(object): 
+    def __init__(self):
+        self._collection={}
+        self._field_index=defaultdict(list)
+        self._tile_index=defaultdict(list)
+        self._color_index=defaultdict(list)
+    
+    def populate(self, folder):
+        for root,dirs,files in os.walk(folder): # DEPTH 1 ONLY
+            for file in files:
+                if file.find('.mjd')!=-1:
+                    the_path = root+"/"+file
+                    self._collection[file] = LightCurve.from_file(the_path)
+    
+    def index(self):
+        for f in self._collection:
+            lc, tilestring, seq, color, _ = f.split('.')
+            field = int(lc.split('_')[-1])
+            tile = int(tilestring)
+            self._field_index[field].append(self._collection[f])
+            self._tile_index[tile].append(self._collection[f])
+            self._color_index[color].append(self._collection[f])
+     
+    #your code here
+    def retrieve(self, facet, value):
+        if facet == 'tile':
+            return self._tile_index[value]
